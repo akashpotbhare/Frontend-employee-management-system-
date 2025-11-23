32bafedc7a4ce9b95e333eb5c4797c6cf37f10bf
akashpotbhare <144005142+akashpotbhare@users.noreply.github.com>
2025-11-23 15:51:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://employeemanagement-system-2822.onrender.com/api',
         changeOrigin: true
       }
     }
